194cc67aedc8b4c389b4db70d9dec10820bb6f97
alex mejias <amejias101@qc.cuny.edu>
2013-01-16 18:07:25 -0500

    progress

--- config.dev.rb
+++ config.dev.rb
@@ -1,4 +1,6 @@
 require 'zurb-foundation'
+# Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,7 +10,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+# output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
